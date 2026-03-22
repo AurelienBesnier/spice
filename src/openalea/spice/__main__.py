e2d6544be828f374df0b2891da1eb5493f8b5566
AurelienBesnier <aurelienbesnier.pro@gmail.com>
2026-03-22 21:13:39 +0100

    Adding executable script

---
+++ src/openalea/spice/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+from openalea.spice.simulator import Simulator
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-c', '--config', type=str, help="The configuration file for the simulation.")
+    args = parser.parse_args()
+
+    sim = Simulator(args.config)
+
+    sim.run()
+
+if __name__ == "__main__":
+    main()
